2a8f11487658766ade306af4f050b1ac6643d880
andrew <soutvoid@gmail.com>
2018-04-22 17:49:09 +0300

    tried tree widget

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <QMdiSubWindow>
 #include <QMainWindow>
 #include <mainwindow.h>
+#include <QTreeWidget>
+#include <QList>
+#include <QTreeWidgetItem>
+#include <QStringList>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +17,14 @@ int main(int argc, char *argv[])
 
     QMdiArea *mdiArea = new QMdiArea();
     QMdiSubWindow *subWindow = new QMdiSubWindow();
+    QTreeWidget *treeWidget = new QTreeWidget();
+    treeWidget->setColumnCount(1);
+    QList<QTreeWidgetItem *> items;
+    for (int i = 0; i < 10; ++i)
+        items.append(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("item: %1").arg(i))));
+    items[0]->addChild(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("item: %1").arg(00))));
+    treeWidget->insertTopLevelItems(0, items);
+    subWindow->setWidget(treeWidget);
     mdiArea->addSubWindow(subWindow);
     MainWindow *mainWindow = new MainWindow();
     mainWindow->setCentralWidget(mdiArea);
